e3dd2e89ee04355e55a90b2868c4425de7c38384
Drewthurm21 <drewthurm21@gmail.com>
2023-07-26 16:46:41 -0500

    added useLocalStorage hook for saving notes to client storage

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,11 +1,11 @@
-
+import NoteForm from './NoteForm'
 
 export default function NewNote() {
 
   return (
     <>
       <h1 className='mb-4'>newNote</h1>
-      <h1> NOTES FORM GOES HERE </h1>
+      <NoteForm />
     </>
   )
 }
