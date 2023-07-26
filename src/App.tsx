1cbbe49634ca76e3108a72ffd6361803665f5feb
Drewthurm21 <drewthurm21@gmail.com>
2023-07-26 17:08:11 -0500

    added onSubmit functions / types for saving notes. Adding tags is next

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useMemo } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import { v4 as uuidV4 } from 'uuid'
+import useLocalStorage from "./useLocalStorage"
 import NewNote from "./NewNote"
 
 export type Tag = {
@@ -30,11 +33,28 @@ export type Note = {
 
 function App() {
 
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes, tags])
+
+  function onCreateNote({tags, ...data}: NoteData) {
+    setNotes( prevNotes => {
+      return [
+        ...prevNotes, 
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)}]
+    })
+  }
+
   return (
     <Container className='my-4'>
       <Routes>
         <Route path='/' element={<h1>Hello.</h1>} />
-        <Route path='/new' element={<NewNote />} />
+        <Route path='/new' element={<NewNote onSubmit={onCreateNote}/>} />
         <Route path=':id'>
           <Route index element={<h1>Show</h1>} />
           <Route path='edit' element={<h1>Edit</h1>} />
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,11 +1,16 @@
 import NoteForm from './NoteForm'
+import { NoteData } from './App'
 
-export default function NewNote() {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void
+}
+
+export default function NewNote({ onSubmit }: NewNoteProps) {
 
   return (
     <>
       <h1 className='mb-4'>newNote</h1>
-      <NoteForm />
+      <NoteForm onSubmit={onSubmit} />
     </>
   )
 }
