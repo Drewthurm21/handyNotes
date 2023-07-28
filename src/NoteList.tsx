14825ab08803af1e08c865e9ad715d8b755db1a6
Drewthurm21 <drewthurm21@gmail.com>
2023-07-28 11:30:35 -0500

    filter logic completed

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
+import { Badge, Button, Card, Col, Form, Row, Stack } from 'react-bootstrap'
 import { Note, Tag } from './App'
 import ReactSelect from 'react-select'
+import style from './NoteList.module.css'
 
 type NoteListProps = {
   availableTags: Tag[]
   notes: Note[]
+}
+
+type SimplifiedNote = {
+  id: string
+  tags: Tag[]
+  title: string
 }
 
 export default function NoteList({ availableTags, notes }: NoteListProps ) {
@@ -14,8 +21,17 @@ export default function NoteList({ availableTags, notes }: NoteListProps ) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([])
   const [title, setTitle] = useState('')
 
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter(note => 
+      (selectedTags.every(tag =>note.tags.some(noteTag => noteTag.id === tag.id))) && 
+      (title.length === 0 || note.title.toLowerCase().includes(title.toLowerCase()))) 
+  }, [selectedTags, title, notes])
+
+  
   return (
     <>
+    <Button onClick={() => console.log(selectedTags, filteredNotes, notes)}></Button>
       <Row>
         <Col><h1>Notes</h1></Col>
         <Col xs='auto'>
@@ -51,8 +67,31 @@ export default function NoteList({ availableTags, notes }: NoteListProps ) {
         </Row>
       </Form>
       <Row xs={1} sm={2} lg={3} xl={4} className='g-3'>
-        
+        {filteredNotes.map(note => (
+          <Col key={note.id}>
+            <NoteCard id={note.id} title={note.title} tags={note.tags} />
+          </Col>
+        ))}
       </Row>
+    </>
+  )
+}
+
+
+function NoteCard({ id, tags, title }: SimplifiedNote) {
+
+  return (
+    <>
+      <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${style.card}`}>
+        <Card.Body>
+          {title}
+        </Card.Body>
+        <Card.Footer className='px-4'> Tags:
+          {tags.map(tag => (
+            <Badge className='mx-2'>{`${tag.label}`}</Badge>
+          ))}
+        </Card.Footer>
+      </Card>
     </>
   )
 }
